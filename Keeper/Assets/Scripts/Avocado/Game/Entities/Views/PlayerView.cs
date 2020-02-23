9b2088122fe4567b89ee0ab04626ce54ea19f7c7
DK <alukardev@gmail.com>
2020-02-23 12:57:05 +0300

    -player animation

--- Keeper/Assets/Scripts/Avocado/Game/Entities/Views/PlayerView.cs
+++ Keeper/Assets/Scripts/Avocado/Game/Entities/Views/PlayerView.cs
@@ -11,6 +11,7 @@ namespace Avocado.Game.Entities.Views {
         
         private static readonly int Idle = Animator.StringToHash("Idle");
         private static readonly int Move = Animator.StringToHash("Move");
+        private static readonly int SpeedMove = Animator.StringToHash("SpeedMove");
 
         protected override void Start() {
             base.Start();
@@ -23,12 +24,14 @@ namespace Avocado.Game.Entities.Views {
         private void Update() {
             if (_mover.Mooving) {
                 _model.SetState(Player.PlayerState.Move);
-                var speed = (Mathf.Abs(_mover.MoveAxis.x) + Mathf.Abs(_mover.MoveAxis.y))/2.0f;
+                var speed =(Mathf.Abs(_mover.MoveAxis.x) + Mathf.Abs(_mover.MoveAxis.y));
                 _animator.SetFloat(Move, speed);
+                _animator.SetFloat(SpeedMove, speed);
             } else {
                 if (_model.CurrentState != Player.PlayerState.Idle) {
                     _model.SetState(Player.PlayerState.Idle);
                     _animator.SetFloat(Move, 0);
+                    _animator.SetFloat(SpeedMove, 0);
                     _animator.SetTrigger(Idle);
                 }
             }
