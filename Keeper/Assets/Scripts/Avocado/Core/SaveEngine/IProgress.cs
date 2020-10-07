2d698ff824578f036759bba59ce4e772c966817d
DK <dmitry.koleev@game-garden.com>
2020-10-07 09:41:20 +0300

    add save engine

---
+++ Keeper/Assets/Scripts/Avocado/Core/SaveEngine/IProgress.cs
@@ -0,0 +1,5 @@
+namespace Avocado.Core.SaveEngine {
+    public interface IProgress {
+        
+    }
+}
---
+++ Keeper/Assets/Scripts/Avocado/Progress/GameProgress.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Avocado.Core.SaveEngine;
+
+namespace Avocado.Progress {
+    [Serializable]
+    public class GameProgress : IProgress {
+        //public int Level { get; set; }
+    }
+}
